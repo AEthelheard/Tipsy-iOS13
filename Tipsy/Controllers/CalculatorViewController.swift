c56d867dfe650651aa0683b6629483b7689fea45
AEthelheard <htang798@usc.edu>
2021-04-03 04:01:54 -0700

    done section 12

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var result = "0.0"
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleWOPercentSign =  String(buttonTitle.dropLast())
+        let buttonTitleDouble = Double(buttonTitleWOPercentSign)!
+        tip = buttonTitleDouble / 100.0
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        billTotal = Double(billTextField.text ?? "0.0") ?? 0.0
+        let needToPay = billTotal * (1 + tip) / Double(numberOfPeople)
+        result = String(format: "%.2f", needToPay)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.number = self.numberOfPeople
+            destinationVC.result = self.result
+            destinationVC.tip = Int(self.tip * 100)
+        }
+    }
+    
+    
+}
+
